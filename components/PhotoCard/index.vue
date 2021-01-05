58a0c84ed1d8993c9e131d7c231c1e0acbe62e0b
Ajeigbe John Oluwaseyi <ajeigbejohnolu@gmail.com>
2021-01-05 09:15:21 +0100

    I fix a responsive bug

--- components/PhotoCard/index.vue
+++ components/PhotoCard/index.vue
@@ -1,123 +1,21 @@
 <template>
   <section class="photo-container">
-    <section v-if="isPhotosArray">
-      <div v-for="photo in photos" :key="photo.id">
-        <div class="photo-container__card">
-          <div class="photo-container__card--photo" @click="popup(photo)">
-            <img :src="photo.urls.full" alt="pic" />
-            <div class="photo-container__card--info">
-              <p class="name">
-                {{ photo.user.first_name }} {{ photo.user.last_name }}
-              </p>
-              <p class="location">{{ photo.user.location }}</p>
-            </div>
-          </div>
+    <div
+      v-for="photo in photos"
+      :key="photo.id"
+      class="photo-container__card"
+      @click="popup(photo)"
+    >
+      <div class="photo-container__card--photo">
+        <img :src="photo.urls.full" alt="pic" />
+        <div class="photo-container__card--info">
+          <p class="name">
+            {{ photo.user.first_name }} {{ photo.user.last_name }}
+          </p>
+          <p class="location">{{ photo.user.location }}</p>
         </div>
       </div>
-      <!-- <div class="photo-container__card">
-        <div class="photo-container__card--photo" @click="popup">
-          <img
-            src="https://i.picsum.photos/id/1015/6000/4000.jpg?hmac=aHjb0fRa1t14DTIEBcoC12c5rAXOSwnVlaA5ujxPQ0I"
-            alt="pic"
-          />
-          <div class="photo-container__card--info">
-            <p class="name">Felix Navidad</p>
-            <p class="location">Location, Nigeria</p>
-          </div>
-        </div>
-      </div>
-
-      <div class="photo-container__card">
-        <div class="photo-container__card--photo" @click="popup">
-          <img
-            src="https://i.picsum.photos/id/1015/6000/4000.jpg?hmac=aHjb0fRa1t14DTIEBcoC12c5rAXOSwnVlaA5ujxPQ0I"
-            alt="pic"
-          />
-          <div class="photo-container__card--info">
-            <p class="name">Felix Navidad</p>
-            <p class="location">Location, Nigeria</p>
-          </div>
-        </div>
-      </div>
-
-      <div class="photo-container__card">
-        <div class="photo-container__card--photo" @click="popup">
-          <img
-            src="https://i.picsum.photos/id/1015/6000/4000.jpg?hmac=aHjb0fRa1t14DTIEBcoC12c5rAXOSwnVlaA5ujxPQ0I"
-            alt="pic"
-          />
-          <div class="photo-container__card--info">
-            <p class="name">Felix Navidad</p>
-            <p class="location">Location, Nigeria</p>
-          </div>
-        </div>
-      </div>
-
-      <div class="photo-container__card">
-        <div class="photo-container__card--photo" @click="popup">
-          <img
-            src="https://i.picsum.photos/id/1015/6000/4000.jpg?hmac=aHjb0fRa1t14DTIEBcoC12c5rAXOSwnVlaA5ujxPQ0I"
-            alt="pic"
-          />
-          <div class="photo-container__card--info">
-            <p class="name">Felix Navidad</p>
-            <p class="location">Location, Nigeria</p>
-          </div>
-        </div>
-      </div>
-
-      <div class="photo-container__card">
-        <div class="photo-container__card--photo" @click="popup">
-          <img
-            src="https://i.picsum.photos/id/1015/6000/4000.jpg?hmac=aHjb0fRa1t14DTIEBcoC12c5rAXOSwnVlaA5ujxPQ0I"
-            alt="pic"
-          />
-          <div class="photo-container__card--info">
-            <p class="name">Felix Navidad</p>
-            <p class="location">Location, Nigeria</p>
-          </div>
-        </div>
-      </div>
-
-      <div class="photo-container__card">
-        <div class="photo-container__card--photo" @click="popup">
-          <img
-            src="https://i.picsum.photos/id/1015/6000/4000.jpg?hmac=aHjb0fRa1t14DTIEBcoC12c5rAXOSwnVlaA5ujxPQ0I"
-            alt="pic"
-          />
-          <div class="photo-container__card--info">
-            <p class="name">Felix Navidad</p>
-            <p class="location">Location, Nigeria</p>
-          </div>
-        </div>
-      </div>
-
-      <div class="photo-container__card">
-        <div class="photo-container__card--photo" @click="popup">
-          <img
-            src="https://i.picsum.photos/id/1015/6000/4000.jpg?hmac=aHjb0fRa1t14DTIEBcoC12c5rAXOSwnVlaA5ujxPQ0I"
-            alt="pic"
-          />
-          <div class="photo-container__card--info">
-            <p class="name">Felix Navidad</p>
-            <p class="location">Location, Nigeria</p>
-          </div>
-        </div>
-      </div>
-
-      <div class="photo-container__card">
-        <div class="photo-container__card--photo" @click="popup">
-          <img
-            src="https://i.picsum.photos/id/1015/6000/4000.jpg?hmac=aHjb0fRa1t14DTIEBcoC12c5rAXOSwnVlaA5ujxPQ0I"
-            alt="pic"
-          />
-          <div class="photo-container__card--info">
-            <p class="name">Felix Navidad</p>
-            <p class="location">Location, Nigeria</p>
-          </div>
-        </div>
-      </div> -->
-    </section>
+    </div>
   </section>
 </template>
 
@@ -130,11 +28,6 @@ export default Vue.extend({
       type: Array,
       default: null,
     },
-  },
-  data() {
-    return {
-      isPhotosArray: Array.isArray(this.photos) && this.photos.length > 0,
-    }
   },
 
   methods: {
